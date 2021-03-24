50e1e64597aa7f485e1bb90ee879b99fa481c5c8
kamrulhasan57 <kamrulhasan570@gmail.com>
2021-03-24 22:41:36 +0600

    viewing All weekday day using switch statement.

--- app.js
+++ app.js
@@ -8,12 +8,26 @@ const port = 3000
 app.get('/', (req, res) => {
 var today = new Date()
 var  day =""
-if(today.getDay() == 6 || today.getDay == 0){
-  day = 'Weekend'
+switch(today.getDay()){
+  case 6:
+  case 0:
+  day ='satudary and sunday and weekend'
+  break;
+  case 1:
+  day='Monday'
+  break;
+  case 2:
+  day='Tuesday'
+  break;
+  case 3:
+  day='wenesday'
+  break;
+  case 4:
+  day='Thusday'
+  default:
+  day= 'friday'
 }
-else{
-  day ='weekday'
-}
+
 
 res.render('list',{kindOfDay: day})
 })
