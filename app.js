a74a7bc3a66ee6b71282adeb206bb382f1e622c4
kamrulhasan57 <kamrulhasan570@gmail.com>
2021-03-24 23:18:22 +0600

    Show Date and list

--- app.js
+++ app.js
@@ -6,30 +6,18 @@ app.set('view engine', 'ejs');
 const port = 3000
 
 app.get('/', (req, res) => {
-var today = new Date()
-var  day =""
-switch(today.getDay()){
-  case 6:
-  case 0:
-  day ='satudary and sunday and weekend'
-  break;
-  case 1:
-  day='Monday'
-  break;
-  case 2:
-  day='Tuesday'
-  break;
-  case 3:
-  day='wenesday'
-  break;
-  case 4:
-  day='Thusday'
-  default:
-  day= 'friday'
-}
+  var today = new Date()
+var options = {
+   weekday: 'long',
+   year: 'numeric',
+   month: 'long',
+   day: 'numeric'
+ }
+ day = today.toLocaleDateString("en-US", options);
 
-
-res.render('list',{kindOfDay: day})
+  res.render('list', {
+    kindOfDay: day
+  })
 })
 
 app.listen(port, () => {
