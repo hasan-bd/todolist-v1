95bd96aa08ea3051646580413c31a29a40379e10
kamrulhasan57 <kamrulhasan570@gmail.com>
2021-03-24 22:14:26 +0600

    Added Embeded java script(Ejs) and Templeting Engine.

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+app.set('view engine', 'ejs');
+
 const port = 3000
 
 app.get('/', (req, res) => {
+var today = new Date()
+var  day =""
+if(today.getDay() == 6 || today.getDay == 0){
+  day = 'Weekend'
+}
+else{
+  day ='weekday'
+}
 
-  var today = new Date()
-  if(today.getDay() === 6 || today.getDay()===0 ){
-    res.write('Hello kamrul \n')
-    res.write('Today weekend . Lets chill !!!')
-
-  }
-  else {
-    res.sendFile(__dirname + '/index.html')
-  }
+res.render('list',{kindOfDay: day})
 })
 
 app.listen(port, () => {
