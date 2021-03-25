abd38aa30366b0e11d61a1e891cd67aee7ffa271
kamrulhasan57 <kamrulhasan570@gmail.com>
2021-03-25 14:39:57 +0600

    Add New Item useing forloop and Embeded java script and Post Request Method.

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const app = express()
-app.set('view engine', 'ejs');
 
+const app = express()
 const port = 3000
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+var items = ['Eat', 'Food', 'Sleep']
 
 app.get('/', (req, res) => {
   var today = new Date()
-var options = {
-   weekday: 'long',
-   year: 'numeric',
-   month: 'long',
-   day: 'numeric'
- }
- day = today.toLocaleDateString("en-US", options);
-
-  res.render('list', {
-    kindOfDay: day
-  })
+  var options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  }
+  day = today.toLocaleDateString("en-US", options);
+  res.render('list', {kindOfDay: day, newitems: items})
 })
+
+
+app.post('/', function(req, res) {
+  var item = req.body.newitem
+  items.push(item)
+  res.redirect('/')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
