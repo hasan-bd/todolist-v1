4ae1166676a7a9b1a75273c9b8c4caab689bebf6
kamrulhasan57 <kamrulhasan570@gmail.com>
2021-03-25 15:14:09 +0600

    Add Stylesheet indside public folder.

--- app.js
+++ app.js
@@ -6,25 +6,26 @@ const port = 3000
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static ('public'))
 
 
-var items = ['Eat', 'Food', 'Sleep']
+let items = ['Eat', 'Food', 'Sleep']
 
 app.get('/', (req, res) => {
-  var today = new Date()
-  var options = {
+  let today = new Date()
+  let options = {
     weekday: 'long',
     year: 'numeric',
     month: 'long',
     day: 'numeric'
   }
   day = today.toLocaleDateString("en-US", options);
-  res.render('list', {kindOfDay: day, newitems: items})
+  res.render('list', {kindOfDay: day, newListItems: items})
 })
 
 
 app.post('/', function(req, res) {
-  var item = req.body.newitem
+  let item = req.body.newItem
   items.push(item)
   res.redirect('/')
 })
